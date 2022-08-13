777715a07fed0bfe28025a28ca067dc0288e3c53
Piyush <pbhirapara97@gmail.com>
2022-08-13 12:04:53 +0530

    Added lab3 and lab4

--- Module1/Java 8/src/lab3/Exercise1.java
+++ Module1/Java 8/src/lab3/Exercise1.java
@@ -1,10 +1,28 @@
 package lab3;
 
+import java.util.Arrays;
+
 public class Exercise1 {
 
+	public static int getSecondSmallest(int[] arr) {
+		int smallest = Integer.MAX_VALUE;
+		for (int num:arr) {
+			if (num < smallest) {
+				smallest = num;
+			}
+		}
+		int secondSmallest = Integer.MAX_VALUE;
+		for (int num:arr) {
+			if (num < secondSmallest && num > smallest) {
+				secondSmallest = num;
+			}
+		}
+		return secondSmallest;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(getSecondSmallest(new int[] {2,3,2,3,4}));
+		System.out.println(getSecondSmallest(new int[] {1,5,4,6,4,1}));
+		System.out.println(getSecondSmallest(new int[] {2,3,2,3,4,1}));
 
 	}
-
 }
