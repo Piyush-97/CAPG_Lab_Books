bbe67ae9d43267e9904e0089625da5ccec4131ca
Piyush <pbhirapara97@gmail.com>
2022-08-22 16:27:29 +0530

    modification and added javascript work

---
+++ Module1/JavaScript/lab1/common.js
@@ -0,0 +1,8 @@
+var msg;
+
+msg = "<p><code>The actual script is in external script file called common.js</code></p>"
+
+function addNos(headVar, bodyVar){
+    document.write(msg);
+    document.write("The sum of the variables headVar and bodyVar is " + (headVar + bodyVar));
+}
---
+++ Module1/JavaScript/lab2/prob2_calCompoundInt.js
@@ -0,0 +1,3 @@
+function calcCompoundInterest(principal, rate, periodInYear){
+    return (principal * (1 + r / 100)^periodInYear) - principal;
+}
